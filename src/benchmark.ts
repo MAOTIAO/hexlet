257a25c1b7324eb35ce57a9d311651fddd698c1d
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-10-28 16:39:20 +0000

    test

---
+++ src/benchmark.ts
@@ -0,0 +1,19 @@
+
+import { wrapSpannedStringByWord } from './wrapSpannedStringByWord';
+
+import Benchmark, { Event } from 'benchmark';
+import { SpannedString } from './SpannedString';
+const suite = new Benchmark.Suite();
+
+const string = SpannedString.create().appendString(
+    'Once upon a midnight dreary'
+);
+
+suite
+    .add('wrapLineByWord', function () {
+        wrapSpannedStringByWord(string, 3);
+    })
+    .on('cycle', function (event: Event) {
+        console.log(String(event.target));
+    })
+    .run({ async: true });
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import { Chalk } from 'chalk';
+import { Theme } from './themes';
+
+export type Config = Theme & {
+    CHALK: Chalk;
+    SCREEN_WIDTH: number;
+    MIN_LINE_WIDTH: number;
+    WRAP_LINES: boolean;
+    HIGHLIGHT_LINE_CHANGES: boolean;
+    SYNTAX_HIGHLIGHTING_THEME?: 
