ad748872f480d186394be036a488e5f0ddd02d79
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-01 07:28:03 +0000

    Change license to MIT

--- src/formatAndFitHunkLine.ts
+++ src/formatAndFitHunkLine.ts
@@ -12,4 +12,15 @@ const LINE_NUMBER_WIDTH = 5;
 export function* formatAndFitHunkLine(
     context: Context,
     fileName: string,
-    
+    lineNo: number,
+    line: string | null,
+    changes: Change[] | null
+): Iterable<FormattedString> {
+    const {
+        BLANK_LINE,
+        LINE_WIDTH,
+        MISSING_LINE_COLOR,
+        DELETED_LINE_COLOR,
+        DELETED_LINE_NO_COLOR,
+        INSERTED_LINE_COLOR,
+       
