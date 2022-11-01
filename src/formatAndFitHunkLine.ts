64091e9b288bb87195d2199ac07a14637fd2d024
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-01 06:02:22 +0000

    Apply gofmt

--- src/formatAndFitHunkLine.ts
+++ src/formatAndFitHunkLine.ts
@@ -2,4 +2,14 @@ import { Change } from 'diff';
 import { Context } from './context';
 import { FormattedString, T } from './formattedString';
 import { highlightChangesInLine } from './highlightChangesInLine';
-import { highlightSyntaxInLine } from './highlightSyntaxInLin
+import { highlightSyntaxInLine } from './highlightSyntaxInLine';
+import { iterFitTextToWidth } from './iterFitTextToWidth';
+import { ThemeColor } from './themes';
+
+// Assuming people aren't editing lines >=100k lines
+const LINE_NUMBER_WIDTH = 5;
+
+export function* formatAndFitHunkLine(
+    context: Context,
+    fileName: string,
+    
