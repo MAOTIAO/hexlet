9078fb9a501fe98dc350b6f97a216d7fec197fbb
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-09 19:59:12 +0000

    Updated Home (markdown)

--- src/getGitConfig.ts
+++ src/getGitConfig.ts
@@ -14,4 +14,16 @@ async function getRawGitConfig() {
     const { stdout } = await execAsync('git config -l');
 
     const rawConfig: Record<string, string> = {};
-    for (const line of stdout.trim().split(
+    for (const line of stdout.trim().split('\n')) {
+        const match = line.match(GIT_CONFIG_LINE_REGEX);
+        if (!match) {
+            continue;
+        }
+        const [, key, value] = match;
+        rawConfig[key] = value;
+    }
+    return rawConfig;
+}
+
+// TODO: Make this less manual
+export a
