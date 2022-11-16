341f5375e445c0698834bd3c5f3dff773bbbc58d
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-16 15:35:21 +0000

    Worked on tests

--- src/highlightSyntaxInLine.ts
+++ src/highlightSyntaxInLine.ts
@@ -1,3 +1,12 @@
 import path from 'path';
 import shiki from 'shiki';
-import { FormattedStrin
+import { FormattedString } from './formattedString';
+import { parseColorDefinition, ThemeColor } from './themes';
+export type HighlightedText = [string, ThemeColor | null];
+
+export function highlightSyntaxInLine(
+    line: FormattedString,
+    fileName: string,
+    highlighter?: shiki.Highlighter
+): void {
+    if (!highligh
