a684ff4c7a69a8aed8370414993568a4c52b2d0b
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-19 21:34:43 +0000

    mod README.md

--- src/iterFitTextToWidth.ts
+++ src/iterFitTextToWidth.ts
@@ -1,1 +1,8 @@
-i
+import { Context } from './context';
+import { FormattedString } from './formattedString';
+import { ThemeColor } from './themes';
+import { wrapSpannedStringByWord } from './wrapSpannedStringByWord';
+
+/**
+ * Wraps or truncates the given line to into the allowed width, depending on
+ * the con
