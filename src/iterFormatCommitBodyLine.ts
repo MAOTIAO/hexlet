0f7861d43732559d323010714815237905a3f686
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-23 20:14:57 +0000

    Make the tests pass

--- src/iterFormatCommitBodyLine.ts
+++ src/iterFormatCommitBodyLine.ts
@@ -1,3 +1,12 @@
 import { Context } from './context';
 import { T, FormattedString } from './formattedString';
-import { iterFitTextToWidth 
+import { iterFitTextToWidth } from './iterFitTextToWidth';
+
+export function* iterFormatCommitBodyLine(
+    context: Context,
+    line: string,
+    isFirstLine: boolean
+): Iterable<FormattedString> {
+    const { COMMIT_TITLE_COLOR, COMMIT_MESSAGE_COLOR, SCREEN_WIDTH } = context;
+
+    const formattedLine =
