e19cb08c191e7106766410a5757578d27d5abed4
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-24 06:38:34 +0000

    More gitignore

--- src/iterFormatCommitBodyLine.ts
+++ src/iterFormatCommitBodyLine.ts
@@ -9,4 +9,16 @@ export function* iterFormatCommitBodyLine(
 ): Iterable<FormattedString> {
     const { COMMIT_TITLE_COLOR, COMMIT_MESSAGE_COLOR, SCREEN_WIDTH } = context;
 
-    const formattedLine =
+    const formattedLine = T().appendString(line);
+
+    if (isFirstLine) {
+        formattedLine.addSpan(0, line.length, COMMIT_TITLE_COLOR);
+    }
+
+    yield* iterFitTextToWidth(
+        context,
+        formattedLine,
+        SCREEN_WIDTH,
+        COMMIT_MESSAGE_COLOR
+    );
+}
