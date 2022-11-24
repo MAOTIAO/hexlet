5cd6922f512111a3a95d8e11d9124e1bb2f5472a
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-24 17:33:21 +0000

    Committing with oXygen XML Web Author

--- src/iterFormatCommitHeaderLine.ts
+++ src/iterFormatCommitHeaderLine.ts
@@ -1,2 +1,12 @@
 import { Context } from './context';
-impo
+import { T, FormattedString } from './formattedString';
+import { iterFitTextToWidth } from './iterFitTextToWidth';
+
+export function* iterFormatCommitHeaderLine(
+    context: Context,
+    line: string
+): Iterable<FormattedString> {
+    const {
+        COMMIT_HEADER_LABEL_COLOR,
+        COMMIT_AUTHOR_COLOR,
+    
