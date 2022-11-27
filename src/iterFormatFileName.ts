214f4ccc4ac6f65c3bd544b8c60d235383fecb67
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-27 11:38:21 +0000

    Minor updates and fixes

--- src/iterFormatFileName.ts
+++ src/iterFormatFileName.ts
@@ -1,1 +1,12 @@
-import { Context } from './context
+import { Context } from './context';
+import { T, FormattedString } from './formattedString';
+import { iterFitTextToWidth } from './iterFitTextToWidth';
+
+export function* iterFormatFileName(
+    context: Context,
+    fileNameA: string,
+    fileNameB: string
+): Iterable<FormattedString> {
+    const {
+        HORIZONTAL_SEPARATOR,
+   
