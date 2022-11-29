80e76510de16df079dfc0b29c49c56eef9a05d7b
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-11-29 05:51:36 +0000

    even more cleanup

--- src/iterFormatFileName.ts
+++ src/iterFormatFileName.ts
@@ -9,4 +9,15 @@ export function* iterFormatFileName(
 ): Iterable<FormattedString> {
     const {
         HORIZONTAL_SEPARATOR,
-   
+        INSERTED_LINE_COLOR,
+        DELETED_LINE_COLOR,
+        INSERTED_LINE_NO_COLOR,
+        DELETED_LINE_NO_COLOR,
+        FILE_NAME_COLOR,
+        SCREEN_WIDTH,
+    } = context;
+
+    yield HORIZONTAL_SEPARATOR;
+
+    const formattedString = T().appendString(' ■■ ');
+    let fileNameL
