26c4c67ee08a6464e8bfebc32f3f7de2a2becb97
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-12-11 21:31:38 +0000

    Updated help text

--- src/previewTheme.ts
+++ src/previewTheme.ts
@@ -1,3 +1,10 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
-import { Readable, Writ
+import { Readable, Writable } from 'stream';
+import terminalSize from 'term-size';
+import { Config } from './config';
+import { getContextForConfig } from './context';
+import { loadTheme } from './themes';
+import { transformContentsStreaming } from './transformContentsStreaming';
+
+const
