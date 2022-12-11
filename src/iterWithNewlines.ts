93e412f8aab84b546d8a32334f56857b10a95032
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-12-11 14:54:30 +0000

    stringify

--- src/iterWithNewlines.ts
+++ src/iterWithNewlines.ts
@@ -1,1 +1,11 @@
-import * as os from 'os
+import * as os from 'os';
+import { Context } from './context';
+
+export async function* iterWithNewlines(
+    context: Context,
+    lines: AsyncIterable<string>
+) {
+    for await (const line of lines) {
+        yield line + os.EOL;
+    }
+}
