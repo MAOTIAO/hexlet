f72f6597b2d93893a7b56b8d0e681305a28ecca8
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-12-18 19:47:27 +0000

    rwsem: simplify __rwsem_do_wake

--- src/transformContentsStreaming.ts
+++ src/transformContentsStreaming.ts
@@ -2,4 +2,13 @@ import stream, { Readable, Writable } from 'stream';
 import { Context } from './context';
 import { iterlinesFromReadable } from './iterLinesFromReadable';
 import { iterReplaceTabsWithSpaces } from './iterReplaceTabsWithSpaces';
-import { iterSideBySideDiffs } from './iterS
+import { iterSideBySideDiffs } from './iterSideBySideDiffs';
+import { iterWithNewlines } from './iterWithNewlines';
+
+export async function transformContentsStreaming(
+    context: Context,
+    input: Readable,
+    output: Writable
+): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const transformedInput 
