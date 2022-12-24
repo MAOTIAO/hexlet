e2e9a7a2efffeb8b7b53aa7ae89f33c56f865d34
MAOTIAO <110644620+MAOTIAO@users.noreply.github.com>
2022-12-24 08:34:33 +0000

    Updated configure.

--- src/wrapSpannedStringByWord.ts
+++ src/wrapSpannedStringByWord.ts
@@ -2,4 +2,15 @@ import { SpannedString } from './SpannedString';
 
 const SPACE_REGEX = /\s/;
 
-function getLineBreaksForString(string: string, width: 
+function getLineBreaksForString(string: string, width: number): number[] {
+    const lineBreaks: number[] = [];
+    let budget = width;
+    let curLineEnd = 0;
+
+    function flushLine() {
+        lineBreaks.push(curLineEnd);
+        budget = width;
+    }
+
+    function pushWord(startIndex: number, endIndex: number) {
+        con
